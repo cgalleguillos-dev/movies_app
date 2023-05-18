20d6fcc6bd71b85757cf62ae26ae08c93c154b3d
Shpm21 <galleguilloscristian2@gmail.com>
2023-05-18 01:26:18 -0400

    refactor: remove unused dtos

--- src/modules/actor/dto/update-actor.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateActorInput } from './create-actor.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateActorInput extends PartialType(CreateActorInput) {
-  @Field(() => Int)
-  id: number;
-}
