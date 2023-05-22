54dda9f9d73dd2d63d3f8a028a6640140f5fafad
Shpm21 <galleguilloscristian2@gmail.com>
2023-05-21 22:04:45 -0400

    feat: add news mutations and query

--- src/modules/playlist/playlist.resolver.ts
+++ src/modules/playlist/playlist.resolver.ts
@@ -1,35 +1,33 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { PlaylistService } from './playlist.service';
 import { Playlist } from 'src/entities';
 import { CreatePlaylistInput } from './dto/create-playlist.input';
 import { UpdatePlaylistInput } from './dto/update-playlist.input';
+import { Body, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { InsertMoviePlaylistInput } from './dto/insert-movie-playlist.input';
 
 @Resolver(() => Playlist)
 export class PlaylistResolver {
   constructor(private readonly playlistService: PlaylistService) { }
 
-  @Mutation(() => Playlist)
-  createPlaylist(@Args('createPlaylistInput') createPlaylistInput: CreatePlaylistInput) {
-    return this.playlistService.create(createPlaylistInput);
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => Playlist, { name: 'addPlaylist' })
+  async addPlaylist(@Context() context, @Args('createPlaylistInput') createPlaylistInput: CreatePlaylistInput) {
+    const userId = context.req.user.id;
+    return await this.playlistService.addPlaylist(userId, createPlaylistInput);
   }
 
-  @Query(() => [Playlist], { name: 'playlist' })
-  findAll() {
-    return this.playlistService.findAll();
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => Playlist, { name: 'insertMoviePlaylist' })
+  async insertMoviePlaylist(@Context() context, @Args('insertMoviePlaylistInput') insertMoviePlaylistInput: InsertMoviePlaylistInput) {
+    return await this.playlistService.insertMoviePlaylist(insertMoviePlaylistInput);
   }
 
-  @Query(() => Playlist, { name: 'playlist' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.playlistService.findOne(id);
-  }
-
-  @Mutation(() => Playlist)
-  updatePlaylist(@Args('updatePlaylistInput') updatePlaylistInput: UpdatePlaylistInput) {
-    return this.playlistService.update(updatePlaylistInput.id, updatePlaylistInput);
-  }
-
-  @Mutation(() => Playlist)
-  removePlaylist(@Args('id', { type: () => Int }) id: number) {
-    return this.playlistService.remove(id);
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [Playlist], { name: 'playlistsByUser' })
+  async findPlaylistsByUser(@Context() context) {
+    const userId = context.req.user.id;
+    return await this.playlistService.findPlaylistsByUser(userId);
   }
 }
